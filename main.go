d53dc95c7e10db950ac2a1ebbfd5ff2efdbf7bcb
nimra98 <armin.jacob@yahoo.de>
2024-08-28 17:17:58 +0200

    add auth token to secure service

--- main.go
+++ main.go
@@ -24,34 +24,68 @@ func main() {
 		log.Print("Port not supplied through ENV, using default port 3000")
 	}
 
+	// Get Auth Token to access this service from ENV variable
+	auth_token := ""
+	if os.Getenv("AUTH_TOKEN") == "" {
+		log.Print("No AUTH_TOKEN provided in ENV.")
+		log.Print("This service will be open to the public!")
+	} else {
+		auth_token = os.Getenv("AUTH_TOKEN")
+		log.Print("AUTH_TOKEN provided in ENV")
+	}
+
+	// Create url structure depending on AUTH_TOKEN
+	// If Auth Token is provided, the URL will be /dyndns/:authtoken/:recordName/:zoneName/:token/:value
+	// If Auth Token is not provided, the URL will be /dyndns/:recordName/:zoneName/:token/:value
+	// The Auth Token will be checked in the handler function
+
 	r := gin.Default()
 
-	r.GET("/dyndns/:recordName/:zoneName/:token/:value", func(c *gin.Context) {
-		token, _ := c.Params.Get("token")
-		zoneName, _ := c.Params.Get("zoneName")
-		recordName, _ := c.Params.Get("recordName")
-		value, _ := c.Params.Get("value")
-		if len(token) == 0 || len(zoneName) == 0 || len(recordName) == 0 || len(value) == 0 {
-			c.String(http.StatusBadRequest, "badreq")
-			log.Printf("badreq - token: %s, zoneName: %s, recordName: %s, value: %s", token, zoneName, recordName, value)
-			return
-		}
-
-		log.Printf("%s-%s-%s", zoneName, recordName, value)
-		dns := hetzner_dns.NewHetznerDNS(token)
-		err := dns.PatchRecord(zoneName, recordName, value)
-		if err != nil {
-			c.String(http.StatusBadRequest, "err")
-			log.Printf("error - error: %s, token: %s, zoneName: %s, recordName: %s, value: %s", err.Error(), token, zoneName, recordName, value)
-			return
-		}
-
-		c.String(http.StatusOK, "OK")
-		log.Printf("ok - token: %s, zoneName: %s, recordName: %s, value: %s", token, zoneName, recordName, value)
-	})
+	// Route abhängig von AUTH_TOKEN erstellen
+	if auth_token == "" {
+		// Route ohne Auth Token
+		r.GET("/dyndns/:recordName/:zoneName/:token/:value", handleRequest)
+	} else {
+		// Route mit Auth Token
+		r.GET("/dyndns/:authToken/:recordName/:zoneName/:token/:value", func(c *gin.Context) {
+			// Auth Token überprüfen
+			if c.Param("authToken") != auth_token {
+				c.String(http.StatusUnauthorized, "unauthorized")
+				return
+			}
+			handleRequest(c)
+		})
+	}
 
 	log.Printf("Starting server on Port " + port + " ...")
 	log.Printf("Translator Version: %s", VERSION)
 
 	r.Run(":" + port)
+}
+
+// Handler-Funktion für die Route
+func handleRequest(c *gin.Context) {
+	token := c.Param("token")
+	zoneName := c.Param("zoneName")
+	recordName := c.Param("recordName")
+	value := c.Param("value")
+
+	if token == "" || zoneName == "" || recordName == "" || value == "" {
+		c.String(http.StatusBadRequest, "badreq")
+		log.Printf("badreq - token: %s, zoneName: %s, recordName: %s, value: %s", token, zoneName, recordName, value)
+		return
+	}
+
+	log.Printf("%s-%s-%s", zoneName, recordName, value)
+	dns := hetzner_dns.NewHetznerDNS(token)
+	err := dns.PatchRecord(zoneName, recordName, value)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "error")
+		log.Printf("Failed to patch DNS record: %v", err)
+		log.Printf("error - error: %s, token: %s, zoneName: %s, recordName: %s, value: %s", err.Error(), token, zoneName, recordName, value)
+		return
+	}
+
+	c.String(http.StatusOK, "success")
+	log.Printf("ok - token: %s, zoneName: %s, recordName: %s, value: %s", token, zoneName, recordName, value)
 }
